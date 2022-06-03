f3544e2cbae8cd27abf318a26ff1004e97593eaf
nnodim <nnodimobinna@gmail.com>
2022-06-03 13:55:17 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,14 @@ int main(void)
 	char i;
 	char x;
 
-	for (i = "a"; i <= "z"; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
 	}
-	for (x = "A"; x <= "Z"; x++)
+	for (x = 'A'; x <= 'Z'; x++)
 	{
 		putchar(x);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
