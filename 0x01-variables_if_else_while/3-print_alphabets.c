f74846c7501724f2e5c5cdc3cebe2c1364076c03
nnodim <nnodimobinna@gmail.com>
2022-06-03 13:43:45 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+	for (char i = "a"; i <= "z"; i++)
+	{
+		putchar(i);
+	}
+	for (char x = "A"; x <= "Z"; x++)
+	{
+		putchar(x);
+	}
+	putchar("\n");
+	return (0);
+}
