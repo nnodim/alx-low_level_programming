b38b9981c4948380e554cddf40ba643e61ef651b
nnodim <nnodimobinna@gmail.com>
2022-06-07 16:20:14 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.c"
+/**
+ * add - prints add two numbers
+ * @i: peint i
+ * @j: print j
+ * Return: Always 0.
+ */
+int add(int i, int j)
+{
+	int k;
+
+	k = i + K;
+	return (k);
+}
