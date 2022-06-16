516d6b7550bde9ec8226bd7be9b4b08f7fcdbdb1
nnodim <nnodimobinna@gmail.com>
2022-06-16 16:46:56 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,7 +11,7 @@ int main(void)
 	p = &n;
 	/*
 	 * write your line of code here...
-	 * * Remember:
+	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
