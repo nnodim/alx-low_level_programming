67104cda8da12bf23f6d1ea83dd32cf6492f0c4c
nnodim <nnodimobinna@gmail.com>
2022-06-22 17:29:25 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,6 +8,8 @@ int factorial(int n)
 {
 	if (n == 0) /*Base condition*/
 		return (1);
+	else if (n < 0) /*Base condition*/
+		return (-1);
 	else
 		return (n * factorial(n - 1)); /*Recursive call*/
 }
