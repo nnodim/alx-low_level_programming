0caf6e9d8f6338c40340295dcc6b2a51d7df70b5
nnodim <nnodimobinna@gmail.com>
2022-06-29 15:09:56 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,15 @@
 #include "main.h"
-int main(void)
+
+/**
+ *_puts - prints a string
+ * @str: string to print
+ * Description: prints a string
+ * On success: return the number of characters printed
+ */
+
+void _puts(char *str)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
 }
